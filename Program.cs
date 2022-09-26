972055992dbaa5f60bf8aea6d5aed5b1b3a2ff1a
Pavel <pavel.sharagin.1994@gmail.com>
2022-09-26 18:31:48 +0500

    task22 done

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+int GetNumber()
+{
+    Console.WriteLine("Введите число ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+void GetPow(int N)
+{
+    int i = 0;
+    while (i <= N)
+    {
+    Console.Write(Math.Pow((i),3)+", ");
+    i ++;
+    }
+}
+GetPow(GetNumber());
